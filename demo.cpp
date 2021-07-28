95dd4d15f07938b8d6d196213586527fb907c6c4
Kevin <vipkhoc377@gmail.com>
2021-07-28 23:50:17 +0700

    up

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
+    system("cls");
     int size;
     cout << "nhap size" << endl;
     cin >> size;
-    cout << size * 10 << endl;
+    cout <<"ket qua: "<< size * 10 << endl;
+    system("pause");
     return 0;
 }
